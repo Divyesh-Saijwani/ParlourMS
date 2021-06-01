6e8828c91a1da2fe4017366d432733993d29da01
Divyesh Saijwani <divyesh.saijwani@rishabhsoft.com>
2021-06-01 10:11:19 +0530

    feat | identity pages added

---
+++ ParlourMS.Web/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using ParlourMS.Data;
+using ParlourMS.Data.Models;
+
+[assembly: HostingStartup(typeof(ParlourMS.Web.Areas.Identity.IdentityHostingStartup))]
+namespace ParlourMS.Web.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
